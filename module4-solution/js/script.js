e9f8409dae0441372e3cd2035cc319cc54f1938f
yookong <yookong3@gmail.com>
2016-04-21 22:34:55 +0900

    use function??

--- module4-solution/js/script.js
+++ module4-solution/js/script.js
@@ -83,10 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl, 
-  function (buildAndShowHomeHTML) {
-    document.querySelector("#main-content")
-      .innerHTML = buildAndShowHomeHTML;
-  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
